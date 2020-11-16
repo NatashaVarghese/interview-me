88ef9bd20bd8a2196191a2bb599682f1b9063c9b
Natasha Varghese <natashavarghese920@gmail.com>
2020-11-16 15:02:25 -0500

    fix liking/disliking

--- src/Questions.js
+++ src/Questions.js
@@ -49,6 +49,18 @@ const Styles = styled.div`
       height: 100%;
       width: auto;
     } 
+
+    .fa-frown:active {
+        color: red
+    }
+    
+    .fa-smile:active {
+        color: yellow
+    }
+    
+    .fa-smile:hover {
+        color: yellow
+    }
 `;
 
 export default class Questions extends Component {
@@ -62,6 +74,8 @@ export default class Questions extends Component {
                 "question": "Given pointer to the head node of a linked list, the task is to reverse the linked list. We need to reverse the list by changing the links between nodes.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 101,
+                "numDisliked": 23,
                 "difficulty": "Medium", 
                 "tags": ["Linked Lists"],
                 "key": 0
@@ -72,6 +86,8 @@ export default class Questions extends Component {
                 "question": "There are n stairs, a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the topGiven a non-negative integer num, repeatedly add all its digits until the result has only one digit.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 57,
+                "numDisliked": 8,
                 "difficulty": "Easy", 
                 "tags": ["Recursion", "Dynamic Programming"],
                 "key": 1
@@ -82,6 +98,8 @@ export default class Questions extends Component {
                 "question": "Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 63,
+                "numDisliked": 14,
                 "difficulty": "Easy", 
                 "tags": ["Stacks"],
                 "key": 2
@@ -92,6 +110,8 @@ export default class Questions extends Component {
                 "question": "Given a string 's' and an integer array 'indices' of the same length. The string 's' will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string. Return the shuffled string.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 5,
+                "numDisliked": 10,
                 "difficulty": "Hard", 
                 "tags": ["Sorting"],
                 "key": 3
@@ -102,6 +122,8 @@ export default class Questions extends Component {
                 "question": "Return the length of the shortest, non-empty, contiguous subarray of A with sum at least K. If there is no non-empty subarray with sum at least K, return -1.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 2,
+                "numDisliked": 5,
                 "difficulty": "Easy", 
                 "tags": ["Queues"],
                 "key": 4
@@ -112,6 +134,8 @@ export default class Questions extends Component {
                 "question": "Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order. Return the sorted array.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 200,
+                "numDisliked": 15,
                 "difficulty": "Medium", 
                 "tags": ["Sorting"],
                 "key": 5
@@ -122,6 +146,8 @@ export default class Questions extends Component {
                 "question": "Say you have an array for which the ith element is the price of a given stock on day i. If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit. Note that you cannot sell a stock before you buy one.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 23,
+                "numDisliked": 51,
                 "difficulty": "Hard", 
                 "tags": ["Linked Lists", "Dynamic Programming"],
                 "key": 6
@@ -132,6 +158,8 @@ export default class Questions extends Component {
                 "question": "Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character. Note that after backspacing an empty text, the text will continue empty.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 68,
+                "numDisliked": 66,
                 "difficulty": "Hard", 
                 "tags": ["Stacks", "Recursion", "Queues"],
                 "key": 7
@@ -142,6 +170,8 @@ export default class Questions extends Component {
                 "question": "Implement a basic calculator to evaluate a simple expression string. The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 7,
+                "numDisliked": 18,
                 "difficulty": "Hard", 
                 "tags": ["Dynamic Programming", "Stacks"],
                 "key": 8
@@ -152,6 +182,8 @@ export default class Questions extends Component {
                 "question": "Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.",
                 "liked": "white",
                 "disliked": "white",
+                "numLiked": 22,
+                "numDisliked": 51,
                 "difficulty": "Medium", 
                 "tags": ["Stacks", "Recursion"],
                 "key": 9
@@ -161,57 +193,64 @@ export default class Questions extends Component {
     }
     
     setDislike(editID) {
-        var editPost = this.state.posts.find( ({key}) => key == editID );
+        var editPost = this.state.posts.find( ({key}) => key === editID );
+        var liked = editPost.liked;
+        var numLiked = editPost.numLiked;
+        var disliked = editPost.disliked;
+        var numDisliked = editPost.numDisliked;
         
-        if(editPost.liked=="white" && editPost.disliked=="white") { 
-            console.log("Here")
-            this.setState({
-                disliked: "red"
-            });
+        if(editPost.liked==="white" && editPost.disliked==="white") { 
+            disliked = "red";
+            numDisliked = numDisliked + 1;
         }
-        else if(editPost.liked=="yellow" && editPost.disliked=="white") { 
-            this.setState({
-                liked: "white",
-                disliked: "red"
-            });
+        else if(editPost.liked==="yellow" && editPost.disliked==="white") { 
+            disliked = "red";
+            liked = "white";
+            numDisliked = numDisliked + 1;
+            numLiked = numLiked - 1;
         }
-        else if(editPost.liked=="white" && editPost.disliked=="red") {
-            this.setState({
-                disliked: "white",
-            });
+        else if(editPost.liked==="white" && editPost.disliked==="red") {
+            disliked = "white";
+            numDisliked = numDisliked - 1;
         }
         
         var newPosts = this.state.posts;
-        newPosts[editID].liked = this.state.liked;
-        newPosts[editID].disliked = this.state.disliked;
+        newPosts[editID].liked = liked;
+        newPosts[editID].numLiked = numLiked;
+        newPosts[editID].disliked = disliked;
+        newPosts[editID].numDisliked = numDisliked;
         this.setState({
             posts: newPosts
         });
     }
     
     setLike(editID) {
-        var editPost = this.state.posts.find( ({key}) => key == editID );
+        var editPost = this.state.posts.find( ({key}) => key === editID );
+        var liked = editPost.liked;
+        var numLiked = editPost.numLiked;
+        var disliked = editPost.disliked;
+        var numDisliked = editPost.numDisliked;
         
-        if (editPost.liked=="white" && editPost.disliked=="white") { 
-            this.setState({
-                liked: "yellow"
-            });
+        if (editPost.liked==="white" && editPost.disliked==="white") { 
+            liked = "yellow";
+            numLiked = numLiked + 1;
         }
-        else if (editPost.liked=="white" && editPost.disliked=="red") { 
-            this.setState({
-                liked: "yellow",
-                disliked: "white"
-            });
+        else if (editPost.liked==="white" && editPost.disliked==="red") { 
+            liked = "yellow";
+            disliked = "white";
+            numLiked = numLiked + 1;
+            numDisliked = numDisliked - 1;
         }
-        else if(editPost.liked=="yellow" && editPost.disliked=="white") {
-            this.setState({
-                liked: "white"
-            });
+        else if(editPost.liked==="yellow" && editPost.disliked==="white") {
+            liked = "white";
+            numLiked = numLiked - 1;
         }
 
         var newPosts = this.state.posts;
-        newPosts[editID].liked = this.state.liked;
-        newPosts[editID].disliked = this.state.disliked;
+        newPosts[editID].liked = liked;
+        newPosts[editID].numLiked = numLiked;
+        newPosts[editID].disliked = disliked;
+        newPosts[editID].numDisliked = numDisliked;
         this.setState({
             posts: newPosts
         });
@@ -238,7 +277,7 @@ export default class Questions extends Component {
                     <label>Try editing your search criteria</label>
                 </div>
                 }
-                {filteredPosts.map(({photo, title, question, liked, disliked, difficulty, tags, key}) => 
+                {filteredPosts.map(({photo, title, question, liked, disliked, numLiked, numDisliked, difficulty, tags, key}) => 
                     <div key={key}>
                         <div class="post" key={key}>
                             <div class="image-cropper">
@@ -251,8 +290,8 @@ export default class Questions extends Component {
                                 <p>{question}</p>
                             </div>
                             <div class="stats">
-                            <FontAwesomeIcon color={liked} onClick={this.setLike.bind(this, key)} icon={faSmile}/><span class="icon-padding"/><span class="icon-padding"/>
-                            <FontAwesomeIcon color={disliked} onClick={this.setDislike.bind(this, key)} icon={faFrown}/><span class="icon-padding"/><span class="icon-padding"/>
+                            <FontAwesomeIcon color={liked} onClick={this.setLike.bind(this,key)} icon={faSmile}/><span class="icon-padding"/>{numLiked}<span class="icon-padding"/>
+                            <FontAwesomeIcon  color={disliked} onClick={this.setDislike.bind(this,key)} icon={faFrown}/><span class="icon-padding"/>{numDisliked}<span class="icon-padding"/>
                             Difficulty: {difficulty}<span class="icon-padding"/>
                             {tags.map((item) => <span>#{item}<span class="icon-padding"/></span>)}
                             </div>
--- src/people.js
+++ src/people.js
@@ -1,42 +1,42 @@
 export const people = [
     {
         "value": "David Carter",
-        "label":<div><img src="images/1.jpg" height="30px" width="30px"/>David Carter</div>
+        "label":<div><img src="images/1.jpg" alt="1" height="30px" width="30px"/>David Carter</div>
     },
     {
         "value": "Carl Weiner",
-        "label":<div><img src="images/2.jpg" height="30px" width="30px"/>Carl Weiner</div>
+        "label":<div><img src="images/2.jpg" alt="2" height="30px" width="30px"/>Carl Weiner</div>
     },
     {
         "value": "Sarah Johnson",
-        "label":<div><img src="images/3.jpg" height="30px" width="30px"/>Sarah Johnson</div>
+        "label":<div><img src="images/3.jpg" alt="3" height="30px" width="30px"/>Sarah Johnson</div>
     },
     {
         "value": "Chad Roberts",
-        "label":<div><img src="images/4.jpg" height="30px" width="30px"/>Chad Roberts</div>
+        "label":<div><img src="images/4.jpg" alt="4" height="30px" width="30px"/>Chad Roberts</div>
     },
     {
         "value": "Natasha Donaldson",
-        "label":<div><img src="images/5.jpg" height="30px" width="30px"/>Natasha Donaldson</div>
+        "label":<div><img src="images/5.jpg" alt="5" height="30px" width="30px"/>Natasha Donaldson</div>
     },
     {
         "value": "Fabian Hoff",
-        "label":<div><img src="images/6.jpg" height="30px" width="30px"/>Fabian Hoff</div>
+        "label":<div><img src="images/6.jpg" alt="6" height="30px" width="30px"/>Fabian Hoff</div>
     },
     {
         "value": "Louisa Alcott",
-        "label":<div><img src="images/7.jpg" height="30px" width="30px"/>Louisa Alcott</div>
+        "label":<div><img src="images/7.jpg" alt="7" height="30px" width="30px"/>Louisa Alcott</div>
     },
     {
         "value": "Stephen Vazquez",
-        "label":<div><img src="images/8.jpg" height="30px" width="30px"/>Stephen Vazquez</div>
+        "label":<div><img src="images/8.jpg" alt="8" height="30px" width="30px"/>Stephen Vazquez</div>
     },
     {
         "value": "Marisa Reilly",
-        "label":<div><img src="images/9.jpg" height="30px" width="30px"/>Marisa Reilly</div>
+        "label":<div><img src="images/9.jpg" alt="9" height="30px" width="30px"/>Marisa Reilly</div>
     },
     {
         "value": "Homer McCullough",
-        "label":<div><img src="images/10.jpg" height="30px" width="30px"/>Homer McCullough</div>
+        "label":<div><img src="images/10.jpg" alt="10" height="30px" width="30px"/>Homer McCullough</div>
     }
 ];
